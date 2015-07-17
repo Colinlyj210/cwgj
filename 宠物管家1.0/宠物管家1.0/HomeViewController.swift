2a92b81171d560b33726c9251a77dccf8dd20034
colinlyj <lyj@lyjs-macbook-pro.local>
2015-07-17 16:13:52 +0800

    1.1.2

--- 宠物管家1.0/宠物管家1.0/HomeViewController.swift
+++ 宠物管家1.0/宠物管家1.0/HomeViewController.swift
@@ -2,14 +2,20 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
+class HomeViewController: UIViewController, CNAutoScrollViewDelegate {
     
     var titleOfOtherPages = ""
-
+    let arr = ["tab1","tab2","tab3","tab4"]
+    var autoScrollView: CNAutoScrollView!
+    
     @IBOutlet var panGesture: UIPanGestureRecognizer!
     override func viewDidLoad() {
         super.viewDidLoad()
-       
+        autoScrollView = CNAutoScrollView(frame: CGRectMake(0, 20, self.view.frame.width, 200))
+        autoScrollView.backgroundColor = UIColor.grayColor()
+        autoScrollView.delegate = self
+        view.addSubview(autoScrollView)
+        autoScrollView.addTimer()
     }
 
     override func didReceiveMemoryWarning() {
@@ -18,7 +24,27 @@ class HomeViewController: UIViewController {
     }
     
 
+    func numbersOfPages() -> Int {
+        return arr.count
+    }
     
+    func imageNameOfIndex(index: Int) -> String! {
+        return arr[index]
+    }
+    
+    func didSelectedIndex(index: Int) {
+        print("you click autoScrollView index:\(index)")
+        //这里写以后要执行的方法
+    }
+    
+    func currentIndexDidChange(index: Int) {
+        print("autoScrollView currentIndex didChange :\(index)")
+    }
+    
+    func indexDidChange(index: Int) {
+        print("scrollView currentIndexDidChange :\(index)")
+    }
+
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
