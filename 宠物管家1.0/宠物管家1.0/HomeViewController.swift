a08dd75e5f6fc0aabc35c81e81615ad5f5019b54
colinlyj <lyj@lyjs-macbook-pro.local>
2015-07-26 09:13:14 +0800

    1.0

--- 宠物管家1.0/宠物管家1.0/HomeViewController.swift
+++ 宠物管家1.0/宠物管家1.0/HomeViewController.swift
@@ -2,7 +2,7 @@
 
 import UIKit
 
-class HomeViewController: UIViewController,SDCycleScrollViewDelegate {
+class HomeViewController: UIViewController {
     
     @IBAction func more(sender: UIBarButtonItem) {
         var frame = sender.valueForKey("view")?.frame
@@ -16,30 +16,15 @@ class HomeViewController: UIViewController,SDCycleScrollViewDelegate {
         print("11111")
     }
     var titleOfOtherPages = ""
-    let arr = ["http://www.lyj210.cn/cwgj/pic/huli/huli.jpg","http://www.lyj210.cn/cwgj/pic/siyang/siyang.jpg","http://www.lyj210.cn/cwgj/pic/xunlian/xunlian.jpg"]
-    let ss = ["网络图片1","网络图片2","网路图片3"]
+    
     
     @IBOutlet var panGesture: UIPanGestureRecognizer!
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let sc = SDCycleScrollView(frame: CGRectMake(0, (self.navigationController?.navigationBar.frame.height)!+20, self.view.frame.width, 415 * self.view.frame.width/650), imageURLStringsGroup: nil)
-        dispatch_async(dispatch_get_main_queue()) { () -> Void in
-            sc.imageURLStringsGroup = self.arr
-        }
-        
-        sc.pageControlAliment = SDCycleScrollViewPageContolAlimentRight
-        sc.titlesGroup = ss
-        sc.delegate = self
-        sc.autoScrollTimeInterval = 2
-        //cycleScrollView2.dotColor = [UIColor yellowColor]; // 自定义分页控件小圆标颜色
-        sc.placeholderImage = UIImage(named: "h1")//网络图片未加载时显示图片h1
-        self.view.addSubview(sc)
+
 
     }
-    func cycleScrollView(cycleScrollView: SDCycleScrollView!, didSelectItemAtIndex index: Int) {
-        print("图片\(index)事件")
-    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
--- 宠物管家1.0/宠物管家1.0/PetsViewController.swift
+++ 宠物管家1.0/宠物管家1.0/PetsViewController.swift
@@ -8,14 +8,27 @@
 
 import UIKit
 
-class PetsViewController: UIViewController {
-
+class PetsViewController: UIViewController,SDCycleScrollViewDelegate {
+    let arr = ["http://www.lyj210.cn/cwgj/pic/huli/huli.jpg","http://www.lyj210.cn/cwgj/pic/siyang/siyang.jpg","http://www.lyj210.cn/cwgj/pic/xunlian/xunlian.jpg"]
+    let ss = ["网络图片1","网络图片2","网路图片3"]
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        let sc = SDCycleScrollView(frame: CGRectMake(0, (self.navigationController?.navigationBar.frame.height)!+20, self.view.frame.width, 415 * self.view.frame.width/650), imageURLStringsGroup: nil)
+        dispatch_async(dispatch_get_main_queue()) { () -> Void in
+            sc.imageURLStringsGroup = self.arr
+        }
+        
+        sc.pageControlAliment = SDCycleScrollViewPageContolAlimentRight
+        sc.titlesGroup = ss
+        sc.delegate = self
+        sc.autoScrollTimeInterval = 2
+        //cycleScrollView2.dotColor = [UIColor yellowColor]; // 自定义分页控件小圆标颜色
+        sc.placeholderImage = UIImage(named: "h1")//网络图片未加载时显示图片h1
+        self.view.addSubview(sc)
     }
-
+    func cycleScrollView(cycleScrollView: SDCycleScrollView!, didSelectItemAtIndex index: Int) {
+        print("图片\(index)事件")
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
