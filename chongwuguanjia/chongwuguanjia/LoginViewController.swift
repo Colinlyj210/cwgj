e58cd16e011d62210b76308ca648fae253d9b825
colinlyj <lyj@lyjs-macbook-pro.local>
2015-08-02 15:11:25 +0800

    ceshi

--- chongwuguanjia/chongwuguanjia/LoginViewController.swift
+++ chongwuguanjia/chongwuguanjia/LoginViewController.swift
@@ -12,7 +12,7 @@ class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        print("aa")
         // Do any additional setup after loading the view.
     }
     override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
